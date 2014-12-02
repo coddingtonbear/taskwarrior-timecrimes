a793d63fd5dfce6e25af523f86d7cb123e02ae96
Adam Coddington <adam.coddington@coxinc.com>
2014-12-01 21:52:52 -0800

    And so it begins...

---
+++ README.rst
@@ -0,0 +1,4 @@
+Taskwarrior Timecrimes
+======================
+
+Travel back in time and undo your duplicate tasks' creation.
---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+
+requirements_path = os.path.join(
+    os.path.dirname(__file__),
+    'requirements.txt',
+)
+try:
+    from pip.req import parse_requirements
+    requirements = [
+        str(req.req) for req in parse_requirements(requirements_path)
+    ]
+except ImportError:
+    requirements = []
+    with open(requirements_path, 'r') as in_:
+        requirements = [
+            req for req in in_.readlines()
+            if not req.startswith('-')
+            and not req.startswith('#')
+        ]
+
+
+setup(
+    name='taskwarrior-timecrimes',
+    version='0.1',
+    url='https://github.com/coddingtonbear/taskwarrior-timecrimes',
+    description=(
+        "Travel back in time so you can undo your duplicate tasks' creation."
+    ),
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'taskwarrior_timecrimes = taskwarrior_timecrimes.cmdline:main'
+        ],
+    },
+)
